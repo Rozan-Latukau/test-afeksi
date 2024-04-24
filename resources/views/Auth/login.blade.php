20935229c45791f1883f49f243b5968776939274
Rozan-Latukau <iyanlatu@gmail.com>
2024-04-24 23:05:20 +0700

    feat:fitur login

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -7,19 +7,20 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/all.css') }}" />
     <title>Login</title>
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-6">
-                <div>
-                    <img src="{{ asset('img/login.jpg') }}" alt="Image Login" class="w-50" />
+    <div class="container d-flex align-items-center">
+        <div class="row shadow-sm  rounded-3 p-3">
+            <div class="col-md-6">
+                <div class="d-flex justify-content-center">
+                    <img src="{{ asset('img/login.jpg') }}" alt="Image Login" class="w-75" />
                 </div>
             </div>
-            <div class="col-6">
-                <h3 class="fw-bold">Afeksi Indonesia</h3>
+            <div class="col-md-6 align-self-center">
+                <h3 class="fw-bold">LOGIN, AFEKSI INDONESIA</h3>
                 <p>Enim pariatur do fugiat occaecat enim.</p>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
